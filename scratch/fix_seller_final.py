c5b29404bac336a6abcee20d9fea7649a390fa2f
Jota7 <jaimeengelmann@gmail.com>
2026-05-15 15:34:20 -0300

    feat: add Seller Activity lens to visualize market participant trends and category distribution

--- scratch/fix_seller_final.py
+++ scratch/fix_seller_final.py
@@ -1,4 +1,6 @@
----
+import sys
+
+content = r'''---
 title: Seller Activity Lens
 toc: false
 ---
@@ -141,3 +143,7 @@ display(html`<div>
   </div>
 
 </div>
+'''
+
+with open('src/lenses/seller.md', 'w', encoding='utf-8', newline='\r\n') as f:
+    f.write(content)
